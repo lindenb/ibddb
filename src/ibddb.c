17a485e7eb655d60c752bdf6fc839b66faf9e5ba
Pierre Lindenbaum <pierre.lindenbaum@univ-nantes.fr>
2014-07-22 18:24:31 +0200

    cont

--- src/ibddb.c
+++ src/ibddb.c
@@ -1,7 +1,32 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Pierre Lindenbaum PhD.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 #include <unistd.h>
 #include <getopt.h>
 #include "ibddb.h"
 
+#define DATASET_DICTIONARY "/dictionary"
 
 static int MarkerCompareByChromName(const void* a,const void *b)
 	{
@@ -29,6 +54,19 @@ static int IndividualCompareByFamName(const void* a,const void *b)
 	int d=strcmp(i->family,  j->family);
 	if(d!=0) return d;
 	return strcmp(i->name,  j->name);
+	}
+
+static ChromPtr findChromosomeByName(ContextPtr ctx,const char* cname)
+	{
+	size_t i=0;
+	for(i=0;i< ctx->chromosome_count;++i)
+		{
+		if( strcmp(ctx->chromosomes[i].name,cname)==0)
+			{
+			return &ctx->chromosomes[i];
+			}
+		}
+	return NULL;
 	}
 
 static IndividualPtr findIndividualByFamName(ContextPtr ctx,const char* fam,const char* indi)
@@ -81,7 +119,7 @@ static void readIbd(ContextPtr ctx)
 	while((line1=gzReadLine(in1,&line_len))!=NULL)
 		{
 		char *line;
-		DEBUG("Opening IBD %s",line1);
+		DEBUG("Step 1: Opening IBD %s",line1);
 		in=safeGZOpen(line1,"r");			
 		
 		while((line=gzReadLine(in,&line_len))!=NULL)
@@ -118,11 +156,11 @@ static void readIbd(ContextPtr ctx)
 				);
 			if(found==NULL)
 				{
-				DEBUG("insert pair %s %s / %s %s (pairs:%d)",
+				/*DEBUG("insert pair %s %s / %s %s (pairs:%d)",
 					tokens[0],tokens[1],
 					tokens[2],tokens[3],
 					ctx->pair_count
-					); 
+					);*/ 
 				ctx->pairs = (PairIndiPtr)safeRealloc(
 					ctx->pairs,
 					sizeof(PairIndi)*(ctx->pair_count+1)
@@ -181,7 +219,12 @@ static void readIbd(ContextPtr ctx)
 
 	
 	{
+	hid_t plist;  /* property list */
+
 	hsize_t  dims[3] = {ctx->pair_count,ctx->marker_count,3};
+
+	
+
 	int dataspace_id =  H5Screate_simple (3,dims, NULL);
 	int dataset_id = H5Dcreate2(
 			ctx->file_id,
@@ -191,13 +234,12 @@ static void readIbd(ContextPtr ctx)
                           H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 	
 
-
 	in1=safeGZOpen(ctx->ibd_filename,"r");
 	while((line1=gzReadLine(in1,&line_len))!=NULL)
 		{
 		ChromPtr prev_chrom=NULL;
 		char *line;
-		DEBUG("Opening IBD %s",line1);
+		DEBUG("Step 2 : Opening IBD %s",line1);
 		in=safeGZOpen(line1,"r");			
 		
 		while((line=gzReadLine(in,&line_len))!=NULL)
@@ -241,19 +283,11 @@ static void readIbd(ContextPtr ctx)
 				}
 				
 			int pair_index=found->index;
-
+			
+			/* get the chromosome and its' index */
 			if(prev_chrom==NULL || strcmp(prev_chrom->name,tokens[4])!=0)
 				{
-				size_t i=0;
-				prev_chrom=NULL;
-				for(i=0;i< ctx->chromosome_count;++i)
-					{
-					if( strcmp(ctx->chromosomes[i].name,tokens[4])==0)
-						{
-						prev_chrom=&ctx->chromosomes[i];
-						break;
-						}
-					}
+				prev_chrom=findChromosomeByName(ctx,tokens[4]);
 				if(prev_chrom==NULL)
 					{
 					DIE_FAILURE("unknown chromosome %s",tokens[4]);
@@ -263,7 +297,7 @@ static void readIbd(ContextPtr ctx)
 			Marker key2;
 			key2.tid=prev_chrom->tid;
 			key2.name=(char*)tokens[5];
-
+			/* get the marker */
 			MarkerPtr marker= (MarkerPtr)bsearch(
 				(const void*)&key2,
 				markersbyname,
@@ -275,19 +309,19 @@ static void readIbd(ContextPtr ctx)
 
 			
 			for(i=0;i< 3;++i)
-				{/*
+				{
 				hsize_t write_start[3] = {pair_index,marker->index,i};
   				hsize_t write_count[3] = {1,1,1};
-				H5Sselect_hyperslab (
+				/* assertGE0(H5Sselect_hyperslab (
 					dataspace_id,
 					H5S_SELECT_SET,
 					write_start,
 					NULL,
 					write_count,
 					NULL
-					);
+					));*/
 				float ibd = atof(tokens[6+i]);
-				H5Dwrite(dataspace_id,
+				/* H5Dwrite(dataspace_id,
 					H5T_NATIVE_FLOAT,
 					H5S_ALL,
 					H5S_ALL,
@@ -305,6 +339,7 @@ static void readIbd(ContextPtr ctx)
 
 	H5Dclose(dataset_id);
 	H5Sclose(dataspace_id);
+
 	}
 
 
@@ -450,12 +485,7 @@ static void readBed(ContextPtr ctx)
 
 		marker->tid=prev_chrom->tid;
 		marker->index=0;
-		marker->name=strdup(tokens[3]);
-		
-		if( marker->name == NULL)
-			{
-			DIE_FAILURE("name == NULL");
-			}
+		marker->name=safeStrDup(tokens[3]);
 		marker->position=atoi(tokens[1]);
 		if(marker->position<=0)
 			{
@@ -569,19 +599,19 @@ static void readFaidx(ContextPtr ctx)
 	int   strtype = H5Tcopy(H5T_C_S1);
 	H5Tset_size (strtype, H5T_VARIABLE);
 	
-	int chromtype = H5Tcreate (H5T_COMPOUND, sizeof (Chrom));
+	hid_t chromtype = H5Tcreate (H5T_COMPOUND, sizeof (Chrom));
 	H5Tinsert(chromtype, "name", HOFFSET(Chrom, name), strtype);
         H5Tinsert(chromtype, "tid", HOFFSET(Chrom, tid), H5T_NATIVE_INT);
  	H5Tinsert(chromtype, "length", HOFFSET(Chrom, length), H5T_NATIVE_INT);
 	
-	
+
 
 	 /* Create the data space for the dataset. */
 	int dataspace_id =  H5Screate_simple (1,dims, NULL);
 	/* create dataset */
 	int dataset_id = H5Dcreate2(
 			ctx->file_id,
-			  "/dictionary",
+			  DATASET_DICTIONARY,
 			  chromtype,
 			  dataspace_id, 
                           H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
@@ -679,11 +709,73 @@ static int main_build(int argc,char** argv)
 	return EXIT_SUCCESS;
 	}
 
+static int main_query(int argc,char** argv)
+	{
+	
+	int status;
+	Context config;
+	memset((void*)&config,0,sizeof(Context));
+	config.argc=argc;
+	config.argv=argv;
+	
+	if(optind+1!=argc)
+		{
+		fprintf(stderr,"Illegal number of arguments.\n");
+		return EXIT_FAILURE;
+		}	
+	config.hdf5_filename=argv[optind];
+	DEBUG("Opening HDF5 file %s",config.hdf5_filename );
+	config.file_id = H5Fopen(config.hdf5_filename, H5F_ACC_RDONLY, H5P_DEFAULT);
+	if( config.file_id < 0)
+		{
+		DIE_FAILURE("H5Fcreate failed err=%d.\n", config.file_id);
+		}
+	
+	DEBUG("Reading " DATASET_DICTIONARY);
+	// http://stackoverflow.com/questions/15786626/get-the-dimensions-of-a-hdf5-dataset
+	int dataset_id =H5Dopen(config.file_id, DATASET_DICTIONARY, H5P_DEFAULT);
+	hid_t dspace = H5Dget_space(dataset_id);
+
+	int ndims = (int)H5Sget_simple_extent_ndims(dspace);
+	int atype  = H5Dget_type(dataset_id); 
+	hsize_t dims[1];
+	H5Sget_simple_extent_dims(dspace, dims, NULL);
+	DEBUG("type = %d dims:%d N=%d\n",atype,ndims,dims[0]);
+	
+	config.chromosome_count=dims[0];
+	config.chromosomes = (ChromPtr)safeCalloc(config.chromosome_count,sizeof(Chrom));
+	
+	H5Dread(dataset_id, atype, H5S_ALL, H5S_ALL, H5P_DEFAULT, config.chromosomes);
+	size_t i;
+for(i=0;i< config.chromosome_count;++i)
+	{
+	DEBUG("%s", config.chromosomes[i].name);
+	}
+      
+
+
+	DEBUG("Closing HDF5 ok");
+
+	H5Sclose(dspace);
+	H5Dclose(dataset_id);
+	DEBUG("Closing HDF5 file");
+	assertGE0(H5Fclose(config.file_id)); 
+	return EXIT_SUCCESS;
+	}
+
+
 int main(int argc,char** argv)
 	{
-	if(argc>1 && strcmp("build",argv[1])==0)
+	if(argc>1)
 		{
-		return main_build(argc-1,&argv[1]);
+		if(strcmp("build",argv[1])==0)
+			{
+			return main_build(argc-1,&argv[1]);
+			}
+		if(strcmp("query",argv[1])==0)
+			{
+			return main_query(argc-1,&argv[1]);
+			}
 		}
 	else
 		{
